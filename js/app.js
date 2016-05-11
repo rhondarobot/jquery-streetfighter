c699a066dcf5bf86b182f2d7325e56ec2402e55e
rhondarobot <rrachellej@gmail.com>
2016-05-11 12:04:20 -0700

    debugged hadouken finish method

--- js/app.js
+++ js/app.js
@@ -11,8 +11,14 @@ $(document).ready(function() {
   	//play hadouken sound
   	$('.ryu-ready').hide();
   	$('.ryu-throwing').show();
-  	$('.hadouken').show();
-  	//animate hadouken to the right of the screen
+  	$('.hadouken').finish().show().animate(
+  		{'left':'1020px'},
+  		500,
+  		function() {
+  			$(this).hide();
+  			$(this).css('left','520px');
+  		}
+  		);
   })	
   .mouseup(function() {
   	$('.ryu-throwing').hide();

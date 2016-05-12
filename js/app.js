14223c8d6d8aa1d272b03011f92faaafd7280cb5
rhondarobot <rrachellej@gmail.com>
2016-05-12 15:28:55 -0700

    hulk green and flipped and trying to get in same white space

--- js/app.js
+++ js/app.js
@@ -4,9 +4,17 @@ $(document).ready(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
   })
+  $('.hulk-ryu').mouseenter(function() {
+    $('.hulk-ryu-still').hide();
+    $('.hulk-ryu-ready').show();
+  })
   .mouseleave(function(){
   	$('.ryu-ready').hide();
   	$('.ryu-still').show();
+  })
+  .mouseleave(function(){
+    $('.hulk-ryu-ready').hide();
+    $('.hulk-ryu-still').show();
   })
   .mousedown(function() {
   	playHadouken();
@@ -19,13 +27,29 @@ $(document).ready(function() {
   			$(this).hide();
   			$(this).css('left','520px');
   		}
-  		);
-  })	
+  		); 
+  })  
+  .mousedown(function() {
+    playHadouken();
+    $('.hulk-ryu-ready').hide();
+    $('.hulk-ryu-throwing').show();
+    $('.hadouken').finish().show().animate(
+      {'right':'1020px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('right','520px');
+      }
+      );
+  })
   .mouseup(function() {
   	$('.ryu-throwing').hide();
   	$('.ryu-ready').show();
   })
-
+  .mouseup(function() {
+    $('.hulk-ryu-throwing').hide();
+    $('.hulk-ryu-ready').show();
+  })
   $("body").keydown(function(x) {
        if(x.keyCode==88){
          $('.ryu-still').hide();
@@ -33,14 +57,10 @@ $(document).ready(function() {
          $('.ryu-cool').show();
        }
   })
-   $("body").keyup(function(x) {
-     if(x.keyCode==88){
-       $('.ryu-still').show();
-       $('.ryu-ready').hide();
-       $('.ryu-cool').hide();
-     }
-   });
-
+ $('.hulk-ryu').mouseenter(function() {
+    $('.hulk-ryu-still').hide();
+    $('.hulk-ryu-ready').show();
+  })
 });
 
 function playHadouken () {
